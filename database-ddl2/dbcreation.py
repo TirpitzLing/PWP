277ba770d9c559ee689fdd9432acaa87c489b549
TirpitzRyou <tirpitzryou@gmail.com>
2026-02-08 10:45:56 +0200

    database creation

--- database-ddl2/dbcreation.py
+++ database-ddl2/dbcreation.py
@@ -8,9 +8,6 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-# --------------------
-# User
-# --------------------
 
 class User(db.Model):
     __tablename__ = "users"
@@ -21,15 +18,15 @@ class User(db.Model):
     email = db.Column(db.String(128), nullable=False, unique=True)
     created_at = db.Column(db.DateTime)
 
-    diet = db.Column(
-        db.String(32),
-        CheckConstraint(
-            "diet IN ('omnivore','vegan','vegetarian','pescatarian',"
-            "'ketogenic','paleo','low_carb','halal','gluten_free','dairy_free')"
-        )
-    )
+    # diet = db.Column(
+    #     db.String(32),
+    #     CheckConstraint(
+    #         "diet IN ('omnivore','vegan','vegetarian','pescatarian',"
+    #         "'ketogenic','paleo','low_carb','halal','gluten_free','dairy_free')"
+    #     )
+    # )
 
-    allergies = db.Column(db.String(128),)
+    allergies = db.Column(db.Text, nullable=True)
 
     recipes = db.relationship("Recipe", back_populates="creator")
     saved_recipes = db.relationship("Save", back_populates="user")
@@ -37,27 +34,15 @@ class User(db.Model):
     # allergies = db.relationship("UserAllergy", back_populates="user")
 
 
-# --------------------
-# Ingredients
-# --------------------
-
 class Ingredient(db.Model):
     __tablename__ = "ingredients"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), nullable=False)
     img_url = db.Column(db.String(255))
+    allergy = db.Column(db.Text)
 
-    type = db.Column(
-        db.String(16),
-        CheckConstraint("type IN ('vegetable','fruit','meat','dairy','spice')")
-    )
-
-    # season = db.Column(
-    #     db.String(16),
-    #     CheckConstraint("season IN ('spring','summer','autumn','winter','all_year')")
-    # )
-
+    # nutrition info
     calories = db.Column(db.Float)
     carbs = db.Column(db.Float)
     protein = db.Column(db.Float)
@@ -77,12 +62,13 @@ class Recipe(db.Model):
 
     servings = db.Column(db.Integer)
     cuisine_type = db.Column(db.String(64))
+    cooking_methods = db.Column(db.Text)
 
-    created_by = db.Column(db.Integer, db.ForeignKey("users.id"))
+    created_by = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)\
+    
     creator = db.relationship("User", back_populates="recipes")
-
     ingredients = db.relationship("RecipeIngredient", back_populates="recipe")
-    cooking_methods = db.relationship("RecipeCookingMethod", back_populates="recipe")
+    # cooking_methods = db.relationship("RecipeCookingMethod", back_populates="recipe")
     saved_by = db.relationship("Save", back_populates="recipe")
 
 
@@ -92,8 +78,8 @@ class RecipeIngredient(db.Model):
     recipe_id = db.Column(db.Integer, db.ForeignKey("recipes.id"), primary_key=True)
     ingredient_id = db.Column(db.Integer, db.ForeignKey("ingredients.id"), primary_key=True)
 
-    amount = db.Column(db.Float)
-    unit = db.Column(db.String(16))
+    amount = db.Column(db.Float) # numeral
+    unit = db.Column(db.String(16)) # g, ml
 
     recipe = db.relationship("Recipe", back_populates="ingredients")
     ingredient = db.relationship("Ingredient", back_populates="recipe_links")
