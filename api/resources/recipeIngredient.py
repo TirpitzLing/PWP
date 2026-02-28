f44521323427b13427de9032ec3b41db530a123c
zhouanqi <angiechou1231@gmail.com>
2026-02-28 14:31:58 +0200

    add recipe ingredient

--- api/resources/recipeIngredient.py
+++ api/resources/recipeIngredient.py
@@ -1,29 +1,39 @@
 from flask import request, Response
 from flask_restful import Resource
 from jsonschema import validate, ValidationError
-from werkzeug.exceptions import BadRequest, UnsupportedMediaType
-from api.extensions import db, api, cache
-from database.dbcreation import Recipe, Ingredient
+from werkzeug.exceptions import BadRequest, UnsupportedMediaType, NotFound
+from api.extensions import db, cache
+from database.dbcreation import Ingredient, RecipeIngredient
+
 
 class RecipeIngredientCollection(Resource):
 
     @cache.cached(timeout=None)
     def get(self, recipe):
+        # list all ingredients for a recipe
         return [i.serialize() for i in recipe.ingredients]
 
     def post(self, recipe):
+        # add a new ingredient to the recipe with amount
         if not request.json:
             raise UnsupportedMediaType
 
         try:
-            validate(request.json, Ingredient.json_schema())
+            validate(request.json, RecipeIngredient.json_schema())
         except ValidationError as e:
             raise BadRequest(description=str(e))
 
-        ingredient = Ingredient()
-        ingredient.deserialize(request.json)
-        recipe.ingredients.append(ingredient)
+        # handle no existing ingredient error
+        ingredient_id = request.json.get("ingredient_id")
+        if not ingredient_id:
+            raise BadRequest(description="missing ingredient_id")
 
+        ingredient = Ingredient.query.get_or_404(ingredient_id)
+
+        assoc = RecipeIngredient(recipe=recipe, ingredient=ingredient)
+        assoc.deserialize(request.json)
+
+        db.session.add(assoc)
         db.session.commit()
 
         return Response(status=201)
@@ -31,9 +41,26 @@ class RecipeIngredientCollection(Resource):
 
 class RecipeIngredientItem(Resource):
 
-    def delete(self, recipe, ingredient):
-        recipe.ingredients.remove(ingredient)
+    def put(self, recipe, ingredient):
+        # update the amount and unit of an ingredient
+        if not request.json:
+            raise UnsupportedMediaType
+
+        assoc = RecipeIngredient.query.filter_by(recipe_id=recipe.id, ingredient_id=ingredient.id).first()
+
+        if not assoc:
+            raise NotFound
+
+        assoc.deserialize(request.json)
         db.session.commit()
         return Response(status=204)
 
+    def delete(self, recipe, ingredient):
+        # remove an ingredient from the recipe
+        assoc = RecipeIngredient.query.filter_by(recipe_id=recipe.id, ingredient_id=ingredient.id).first()
 
+        if assoc:
+            db.session.delete(assoc)
+            db.session.commit()
+
+        return Response(status=204)
