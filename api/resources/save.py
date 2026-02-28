4df115bfdac0a35f8b9673e2d669631fbc88ebaa
zhouanqi <angiechou1231@gmail.com>
2026-02-28 15:19:40 +0200

    decided to use 409

--- api/resources/save.py
+++ api/resources/save.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flask import request, Response
 from flask_restful import Resource
-from werkzeug.exceptions import BadRequest
+from werkzeug.exceptions import BadRequest, Conflict
 from database.dbcreation import Recipe, Save
 from api.extensions import db, api
 
@@ -22,15 +22,15 @@ class SaveCollection(Resource):
 
         existing = Save.query.filter_by(user_id=user.id, recipe_id=recipe.id).first()
 
-        if not existing:
-            new_save = Save(user_id=user.id, recipe_id=recipe.id, created_at=datetime.utcnow())
-            db.session.add(new_save)
-            db.session.commit()
-            # successfully created
-            return Response(status=201, headers={"Location": api.url_for(SaveItem, user=user, recipe=recipe)})
+        if existing:
+            raise Conflict(description="Recipe already saved by this user")
 
-        # already exist, return 204
-        return Response(status=204)
+        new_save = Save(user_id=user.id, recipe_id=recipe.id, created_at=datetime.utcnow())
+        db.session.add(new_save)
+        db.session.commit()
+
+        # successfully created
+        return Response(status=201, headers={"Location": api.url_for(SaveItem, user=user, recipe=recipe)})
 
 
 class SaveItem(Resource):
