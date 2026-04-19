e5fbf6e968e507ad0f7b5a7e9b40e7ab7571819d
TyleR <tianyi11347@gmail.com>
2026-04-20 07:26:27 +0800

    cors expose location

--- dbms/api.py
+++ dbms/api.py
@@ -22,7 +22,7 @@ from dbms.resources.token import Token
 
 api_bp = Blueprint("api", __name__, url_prefix="/api")
 CORS(api_bp)
-api = Api(api_bp)
+CORS(api_bp, expose_headers=["Location"])
 
 
 # register routes
