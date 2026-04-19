2621fa5506704fb3c30e7624d1e9a69622516496
TirpitzRyou <tirpitzryou@gmail.com>
2026-04-19 18:44:30 +0300

    report generator outline

--- dbms/api.py
+++ dbms/api.py
@@ -6,6 +6,10 @@ from flask import Blueprint
 from flask_restful import Api
 from dbms.resources.recipe import RecipeCollection, RecipeItem, RecipeNutrition
 from dbms.resources.ingredient import IngredientCollection, IngredientItem
+from dbms.resources.report import (
+    Report,
+    ReportDownload,
+)
 from dbms.resources.user import UserCollection, UserItem, UserRecipeCollection
 from dbms.resources.recipe_ingredient import (
     RecipeIngredientCollection,
@@ -34,6 +38,10 @@ api.add_resource(
 api.add_resource(UserCollection, "/users/")
 api.add_resource(UserItem, "/users/<user:user>/")
 api.add_resource(UserRecipeCollection, "/users/<user:user>/recipes/")
+api.add_resource(Report, "/users/<user:user>/reports/")
+api.add_resource(
+    ReportDownload, "/users/<user:user>/reports/<int:report_job_id>/download/"
+)
 
 api.add_resource(SaveCollection, "/users/<user:user>/saves/")
 api.add_resource(SaveItem, "/users/<user:user>/saves/<recipe:recipe>/")
